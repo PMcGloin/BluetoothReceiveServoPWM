2495d02d7814e0f7679dd4b6d6cb58e66f81025c
Padraig McGloin <S00173201@mail.itsligo.ie>
2019-05-01 19:23:17 +0100

    Initial Commit

--- uart.h
+++ uart.h
@@ -1,22 +1,3 @@
-char UARTInitialise(const long int baudrate){
-	unsigned int x;
-	x = (_XTAL_FREQ - baudrate*64)/(baudrate*64);
-	if(x>255){
-		x = (_XTAL_FREQ - baudrate*16)/(baudrate*16);
-		BRGH = 1;
-	}
-	if(x<256){
-        SPBRG = x;
-        SYNC = 0;
-        SPEN = 1;
-        TRISC7 = 1;
-        TRISC6 = 1;
-        CREN = 1;
-        TXEN = 1;
-        return 1;
-	}
-	return 0;
-}
 char UARTReadChar(){
     while(!RCIF);
     if (OERR == 1){ //overrun error
